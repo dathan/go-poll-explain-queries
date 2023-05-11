68458d09b87a219dceb99b0673e3bd26932d1fcc
Dathan Vance Pattishall <dathanvp@gmail.com>
2023-05-11 12:52:29 -0700

    feat: support for a single control-c to exit using the context

--- cmd/go-poll-explain-queries/main.go
+++ cmd/go-poll-explain-queries/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"os"
@@ -59,8 +60,16 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 
+	// Create a context that we can cancel
+	ctx, cancel := context.WithCancel(context.Background())
 	var wg sync.WaitGroup
 	wg.Add(2) // We have 2 goroutines
+
+	go func() {
+		// Listen for the interrupt signal, and when it's received, cancel the context
+		<-c
+		cancel()
+	}()
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", os.Getenv("MYSQL_USERNAME"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_DATABASE"))
 
@@ -73,63 +82,75 @@ func main() {
 	}
 	defer db.Close()
 
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case <-c:
-				fmt.Println("Quitting... processlist")
-				return
-			default:
-				// Poll the processlist
-				rows, err := db.Query("SHOW FULL PROCESSLIST")
+	go pollProcessList(ctx, &wg, db)
+	go detectLocks(ctx, &wg, db)
+
+	// Block the main thread until an interrupt signal is received
+	wg.Wait()
+	// Both goroutines have finished, now we can exit
+	fmt.Println("Both goroutines have shut down, exiting...")
+	os.Exit(0)
+}
+
+// poll the processList and explain bad queries
+func pollProcessList(ctx context.Context, wg *sync.WaitGroup, db *sql.DB) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Quitting... processlist")
+			return
+		default:
+			// Poll the processlist
+			rows, err := db.Query("SHOW FULL PROCESSLIST")
+			if err != nil {
+				panic(err)
+			}
+
+			for rows.Next() {
+				var process Process
+				err := rows.Scan(&process.Id, &process.User, &process.Host, &process.Db, &process.Command, &process.Time, &process.State, &process.Info)
+
 				if err != nil {
 					panic(err)
 				}
 
-				for rows.Next() {
-					var process Process
-					err := rows.Scan(&process.Id, &process.User, &process.Host, &process.Db, &process.Command, &process.Time, &process.State, &process.Info)
-
+				// If the query has been running for more than 2 seconds, run EXPLAIN
+				if process.Time > 2 && process.Info.Valid {
+					utils.PrettyPrint(process)
+					query := fmt.Sprintf("EXPLAIN %s", process.Info.String)
+					explain, err := db.Query(query)
 					if err != nil {
 						panic(err)
 					}
-
-					// If the query has been running for more than 2 seconds, run EXPLAIN
-					if process.Time > 2 && process.Info.Valid {
-						utils.PrettyPrint(process)
-						query := fmt.Sprintf("EXPLAIN %s", process.Info.String)
-						explain, err := db.Query(query)
+					for explain.Next() {
+						// Print the output of the EXPLAIN query
+						var output Explain
+						err := explain.Scan(&output.Id, &output.SelectType, &output.Table, &output.Partitions, &output.Type, &output.PossibleKeys, &output.Key, &output.KeyLen, &output.Ref, &output.Rows, &output.Filtered, &output.Extra)
 						if err != nil {
 							panic(err)
 						}
-						for explain.Next() {
-							// Print the output of the EXPLAIN query
-							var output Explain
-							err := explain.Scan(&output.Id, &output.SelectType, &output.Table, &output.Partitions, &output.Type, &output.PossibleKeys, &output.Key, &output.KeyLen, &output.Ref, &output.Rows, &output.Filtered, &output.Extra)
-							if err != nil {
-								panic(err)
-							}
-							utils.PrettyPrint(output)
-						}
+						utils.PrettyPrint(output)
 					}
 				}
-
-				time.Sleep(1 * time.Second)
 			}
-		}
-	}()
 
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case <-c:
-				fmt.Println("Quitting... lock detector")
-				return
-			default:
-				// Construct and execute the query
-				rows, err := db.Query(`
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+// detect locks and print them out
+func detectLocks(ctx context.Context, wg *sync.WaitGroup, db *sql.DB) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Quitting... lock detector")
+			return
+		default:
+			// Construct and execute the query
+			rows, err := db.Query(`
 				SELECT   
 					th.PROCESSLIST_ID,   
 					th.NAME,   
@@ -153,47 +174,38 @@ func main() {
 				GROUP BY   
 					dl.ENGINE_LOCK_ID
 			`)
+			if err != nil {
+				panic(err)
+			}
+
+			// Iterate over the rows
+			for rows.Next() {
+				var result QueryResult
+				err := rows.Scan(
+					&result.ProcessListId,
+					&result.Name,
+					&result.Type,
+					&result.ProcessListState,
+					&result.ProcessListInfo,
+					&result.ProcessListTime,
+					&result.ObjectSchema,
+					&result.ObjectName,
+					&result.LockMode,
+					&result.LockStatus,
+					&result.WaitingThreads,
+				)
 				if err != nil {
 					panic(err)
 				}
 
-				// Iterate over the rows
-				for rows.Next() {
-					var result QueryResult
-					err := rows.Scan(
-						&result.ProcessListId,
-						&result.Name,
-						&result.Type,
-						&result.ProcessListState,
-						&result.ProcessListInfo,
-						&result.ProcessListTime,
-						&result.ObjectSchema,
-						&result.ObjectName,
-						&result.LockMode,
-						&result.LockStatus,
-						&result.WaitingThreads,
-					)
-					if err != nil {
-						panic(err)
-					}
-
-					// Print the result if the conditions are met
-					if result.ProcessListTime >= 3 || result.WaitingThreads >= 2 {
-						utils.PrettyPrint(result)
-					}
+				// Print the result if the conditions are met
+				if result.ProcessListTime >= 3 || result.WaitingThreads >= 2 {
+					utils.PrettyPrint(result)
 				}
-
-				// Sleep for 10 seconds before the next iteration
-				time.Sleep(10 * time.Second)
 			}
-		}
-	}()
 
-	// Block the main thread until an interrupt signal is received
-	<-c
-	// We received an interrupt signal, now wait for both goroutines to finish
-	wg.Wait()
-	// Both goroutines have finished, now we can exit
-	fmt.Println("Both goroutines have shut down, exiting...")
-	os.Exit(0)
+			// Sleep for 10 seconds before the next iteration
+			time.Sleep(10 * time.Second)
+		}
+	}
 }
