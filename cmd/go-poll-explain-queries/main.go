399fc948dea031e6b7951e0f21c448415ac1bcfa
Dathan Vance Pattishall <dathanvp@gmail.com>
2023-06-01 19:57:26 -0700

    fix: let's remove the valid and rename a var

--- cmd/go-poll-explain-queries/main.go
+++ cmd/go-poll-explain-queries/main.go
@@ -29,11 +29,11 @@ func main() {
 	// take in inputs
 	kill := flag.Bool("kill", false, "kill any slow query that bypasses the slowis threshold")
 	slowis := flag.Int("slowis", 600, "slowis the threshold in seconds that a query needs to take for it to be considered slow")
-	lockThreshold := flag.Int("row_threshold", 1000, "row_threshold is the number of rows in ready to rollback state for a long running transaction")
+	rowThreshold := flag.Int("row_threshold", 1000, "row_threshold is the number of rows in ready to rollback state for a long running transaction")
 	batchMode := flag.Bool("batch", true, "Only run this application once, do not act a daemon")
 	flag.Parse()
 
-	poll := db_health.NewHealth(ctx, &wg, *slowis, *lockThreshold, *kill, *batchMode)
+	poll := db_health.NewHealth(ctx, &wg, *slowis, *rowThreshold, *kill, *batchMode)
 	wg.Add(1)
 	go poll.PollProcessAndLongRunningTrx()
 	// add the other type of queries below in a go routine with context
